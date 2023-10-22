5f63b53c0a6e1fd3411b32e4b335807b9aae6b56
ChetnaGupta-tech <74051849+ChetnaGupta-tech@users.noreply.github.com>
2023-10-22 16:46:59 +0530

    Add files via upload

---
+++ scripts/common.js
@@ -0,0 +1,17 @@
+function openSignupModal() {
+    document.getElementById("signUpModal").style.display="block";
+}
+
+function openSigninModal() {
+    document.getElementById("signInModal").style.display="block";
+}
+
+function closeModal() {
+    document.getElementById("signUpModal").style.display="none";
+    document.getElementById("signInModal").style.display="none";
+}
+
+function displaySignupModal() {
+    document.getElementById("signUpModal").style.display="block";
+    document.getElementById("signInModal").style.display="none";
+}
